01003efc8864093dd5c087402dbbe64ac1871e38
vishwajeet161 <vishwajeet.patel161@gmail.com>
2025-09-19 23:56:41 +0530

    Created a structured way saperate the modules

---
+++ frontend/journeo-ui/src/app/modules/shared-module/shared-module.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shared-module',
+  imports: [],
+  templateUrl: './shared-module.component.html',
+  styleUrl: './shared-module.component.scss'
+})
+export class SharedModuleComponent {
+
+}
---
+++ frontend/journeo-ui/src/app/modules/train-ticket/train-ticket.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-train-ticket',
+  imports: [],
+  templateUrl: './train-ticket.component.html',
+  styleUrl: './train-ticket.component.scss'
+})
+export class TrainTicketComponent {
+
+}
