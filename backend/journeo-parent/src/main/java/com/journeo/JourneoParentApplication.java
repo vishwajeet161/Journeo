e1f5dd3a42ce4957029641ba3816d9a779845da1
vishwajeet161 <vishwajeet.patel161@gmail.com>
2025-10-01 01:43:39 +0530

    started with the structure of the backend services

---
+++ backend/journeo-parent/src/main/java/com/journeo/JourneoParentApplication.java
@@ -0,0 +1,13 @@
+package com.journeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JourneoParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JourneoParentApplication.class, args);
+	}
+
+}
---
+++ backend/service-discovery/src/test/java/com/journeo/discovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.journeo.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
