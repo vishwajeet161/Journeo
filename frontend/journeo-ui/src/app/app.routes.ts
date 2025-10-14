a9ac4a936fbacb08d0c79329c9df234f0b632747
vishwajeet161 <vishwajeet.patel161@gmail.com>
2025-10-15 01:20:40 +0530

    generated a new component  and added to the routing

--- frontend/journeo-ui/src/app/app.routes.ts
+++ frontend/journeo-ui/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { LandingPageComponent } from './modules/landing/landing-page/landing-pag
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },
-//   { path: 'train', loadComponent: () => import('./modules/train-ticket/train-ticket.component').then(m => m.TrainTicketComponent) },
+  { path: 'train', loadComponent: () => import('./modules/train-ticket-module/train-ticket/train-ticket.component').then(m => m.TrainTicketComponent) },
 //   { path: 'bus', loadComponent: () => import('./modules/bus-ticket/bus-ticket.component').then(m => m.BusTicketComponent) },
 //   { path: 'flight', loadComponent: () => import('./modules/flight-ticket/flight-ticket.component').then(m => m.FlightTicketComponent) },
   { path: 'login', loadComponent: () => import('./modules/login-module/login/login.component').then(m => m.LoginComponent) },
---
+++ frontend/journeo-ui/src/app/modules/train-ticket-module/train-ticket/train-ticket.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'journeo-train-ticket',
+  imports: [],
+  templateUrl: './train-ticket.component.html',
+  styleUrl: './train-ticket.component.scss'
+})
+export class TrainTicketComponent {
+
+}
