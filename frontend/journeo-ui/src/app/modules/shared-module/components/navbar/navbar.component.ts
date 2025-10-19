4f94f3f8c7a408a926c69ec9e05b03aa3ac54a45
vishwajeet161 <vishwajeet.patel161@gmail.com>
2025-10-19 21:28:13 +0530

    worked on login page

--- frontend/journeo-ui/src/app/modules/shared-module/components/navbar/navbar.component.ts
+++ frontend/journeo-ui/src/app/modules/shared-module/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'journeo-navbar',
@@ -11,11 +12,12 @@ import { CommonModule } from '@angular/common';
 export class NavbarComponent {
   @Input() conf:any;
   isLoggedIn = false; // Later replace with AuthService
-
-  constructor() {}
+  constructor(private router: Router ) {
+  }
 
   ngOnInit() {
     console.log(this.conf);
+    console.log(this.router.url);
     this.isLoggedIn = this.conf?.isLoggedIn || false;
   }
 }
