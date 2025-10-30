f78f22699ecae0491212ad4d802cead836e6b053
vishwajeet161 <vishwajeet.patel161@gmail.com>
2025-10-30 23:45:30 +0530

    registration page added

--- frontend/journeo-ui/src/app/app.routes.ts
+++ frontend/journeo-ui/src/app/app.routes.ts
@@ -6,6 +6,7 @@ export const routes: Routes = [
   { path: 'train', loadComponent: () => import('./modules/train-ticket-module/train-ticket/train-ticket.component').then(m => m.TrainTicketComponent) },
 //   { path: 'bus', loadComponent: () => import('./modules/bus-ticket/bus-ticket.component').then(m => m.BusTicketComponent) },
 //   { path: 'flight', loadComponent: () => import('./modules/flight-ticket/flight-ticket.component').then(m => m.FlightTicketComponent) },
-  { path: 'login', loadComponent: () => import('./modules/login-module/login/login.component').then(m => m.LoginComponent) },
+  { path: 'login', loadComponent: () => import('./modules/user-module/login/login.component').then(m => m.LoginComponent) },
+  {path: 'register', loadComponent: () => import('./modules/user-module/user-registration/user-registration.component').then(m => m.UserRegistrationComponent)},
   { path: '**', redirectTo: '' }
 ];
