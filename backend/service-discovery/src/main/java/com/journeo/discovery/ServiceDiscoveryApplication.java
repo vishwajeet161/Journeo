9ea29a0dd50ec73ad5bb6233f0a5c52033089210
vishwajeet161 <vishwajeet.patel161@gmail.com>
2025-12-07 00:43:36 +0530

    eureka server is now running with api-gateway

--- backend/service-discovery/src/main/java/com/journeo/discovery/ServiceDiscoveryApplication.java
+++ backend/service-discovery/src/main/java/com/journeo/discovery/ServiceDiscoveryApplication.java
@@ -2,8 +2,10 @@ package com.journeo.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceDiscoveryApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +13,4 @@ public class ServiceDiscoveryApplication {
 	}
 
 }
+
