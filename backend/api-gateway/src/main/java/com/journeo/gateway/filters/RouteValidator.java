25f0e2c64643751df079929524281ee6be396c82
vishwajeet161 <vishwajeet.patel161@gmail.com>
2025-12-26 17:09:23 +0530

    fixed the issue raised regarding the api_gateway service is not working

--- backend/api-gateway/src/main/java/com/journeo/gateway/filters/RouteValidator.java
+++ backend/api-gateway/src/main/java/com/journeo/gateway/filters/RouteValidator.java
@@ -2,23 +2,25 @@ package com.journeo.gateway.filters;
 
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 
 import java.util.List;
 import java.util.function.Predicate;
 
 @Component
 public class RouteValidator {
-    
+
+    private static final AntPathMatcher matcher = new AntPathMatcher();
+
     public static final List<String> openApiEndpoints = List.of(
-        "/api/auth/register",
-        "/api/auth/login",
-        "/eureka",
-        "/eureka/**",
-        "/actuator/health"
+            "/api/auth/**",
+            "/eureka/**",
+            "/actuator/**"
     );
-    
+
     public Predicate<ServerHttpRequest> isSecured =
-        request -> openApiEndpoints
-            .stream()
-            .noneMatch(uri -> request.getURI().getPath().contains(uri));
+            request -> openApiEndpoints
+                    .stream()
+                    .noneMatch(pattern ->
+                            matcher.match(pattern, request.getURI().getPath()));
 }
